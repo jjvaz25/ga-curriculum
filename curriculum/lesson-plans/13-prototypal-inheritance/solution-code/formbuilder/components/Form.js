8b89cef95cc3bbe9f48ab6c968d53fdab8273ccb
kyle liu <kyle116@yahoo.com>
2021-03-18 16:55:56 -0700

    udpated to solution

--- curriculum/lesson-plans/13-prototypal-inheritance/solution-code/formbuilder/components/Form.js
+++ curriculum/lesson-plans/13-prototypal-inheritance/solution-code/formbuilder/components/Form.js
@@ -7,10 +7,12 @@ import Input from './Input.js';
 //  <button class="btn btn-primary">Submit</button>
 // </form>
 const Form = function(title, inputs, onSubmit) {
+  // create Input for every input in inputs
   this.inputs = inputs.map((input) => {
     return new Input(input.name, input.type, input.label);
   });
   
+  // create form element
   const formElement = document.createElement('form');
 
   // create title
@@ -23,13 +25,17 @@ const Form = function(title, inputs, onSubmit) {
     formElement.append(input.element);
   });
 
+  // append all inputs to form
   const submitButton = document.createElement('button');
   submitButton.setAttribute('type', 'submit');
+  // create button with type submit adn classes btn, btn-primary
   submitButton.classList.add('btn', 'btn-primary');
+  // add text Submit to button
   submitButton.textContent = 'Submit';
+  // append submit to form
   formElement.append(submitButton);
 
-  // add event handler
+  // BONUS: add submit event handler to form
   formElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
@@ -42,6 +48,7 @@ const Form = function(title, inputs, onSubmit) {
   this.onSubmit = onSubmit;
   this.element = formElement;
 };
+// BONUS: get current values
 Form.prototype.getCurrentValues = function() {
   const inputEntries = this.inputs.map((input) => {
     return [
